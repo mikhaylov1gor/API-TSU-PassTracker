eecedcc86993172b34057f8b699a0c0dbe53ef8e
Sunrize1 <vladislaw.2000.pro@gmail.com>
2025-02-21 21:29:24 +0700

    all models created and db configured

---
+++ API-TSU-PassTracker/Models/DTO/UserModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_TSU_PassTracker.Models.DTO
+{
+    public class UserModel
+    {
+        
+    }
+}
